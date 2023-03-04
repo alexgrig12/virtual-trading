c0f8f24b4bd33c3ef9c44c9e616df88fbddc3c59
PasararAndrey <pasararandrey@gmail.com>
2023-03-04 23:55:58 +0200

    Added coins and dashboard fragment and layouts

---
+++ feature/coins/src/main/java/virtualtrading/coins/api/CoinsFragment.kt
@@ -0,0 +1,7 @@
+package virtualtrading.coins.api
+
+import androidx.fragment.app.Fragment
+import virtualtrading.coins.R
+
+class CoinsFragment : Fragment(R.layout.fragment_coins) {
+}
---
+++ feature/dashboard/src/main/java/virtualtrading/dashboard/api/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package virtualtrading.dashboard.api
+
+import androidx.fragment.app.Fragment
+import virtualtrading.dashboard.R
+
+class DashboardFragment: Fragment(R.layout.fragment_dashboard) {
+}
